2489fc7bccef6283ecd16aa76c0d181d01765f24
Harsimran Kaur <harsimrankar19@gmail.com>
2024-05-18 13:28:38 +0530

    added responsiveness and header

--- src/screens/ProductList.jsx
+++ src/screens/ProductList.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { getRequest, postRequest } from "../axios.js";
 import { useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import "./ProductList.css";
+import Logo from "../assets/logo_green1.png";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
@@ -23,12 +26,30 @@ const ProductList = () => {
 
   return (
     <div className="container">
+      <div className="header">
+        <div className="logo_heading">
+          <img src={Logo} alt="" className="logo" />
+          <p className="heading">
+            <i>Product Catalogue</i>
+          </p>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Search your product here..."
+          className="search"
+        />
+        <button className="add_product">Add Product</button>
+      </div>
       <div className="product_container">
         {products && products.length
           ? products.map((item) => (
               <div className="product" key={item.id}>
                 <img src={item.productImage} alt={item.name} />
-                {/* <p>{item.title}</p> */}
+                <div className="essentials">
+                  <p>{item.name}</p>
+                  <div>&#8377;{item.selling_price}</div>
+                </div>
               </div>
             ))
           : null}
